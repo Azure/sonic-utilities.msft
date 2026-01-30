7d101b6a5c8bc74c148fa0d522754f9f1303180d
Ubuntu <schikkalawar@schikkalawar-dev-vm.d4y3nv5wwgfelhhopdxv1tqjld.dx.internal.cloudapp.net>
2026-01-30 05:02:28 +0000

    renaming files

--- show_gnmi_util/formatter/__init__.py
+++ show_gnmi_util/formatter/__init__.py
@@ -4,6 +4,6 @@ Formatters for different show commands
 
 from .show_version_formatter import ShowVersionFormatter
 from .show_vlan_brief_formatter import ShowVlanBriefFormatter
-from .show_clock import ShowClockFormatter, ShowClockTimezonesFormatter
+from .show_clock_formatter import ShowClockFormatter, ShowClockTimezonesFormatter
 
 __all__ = ['ShowVersionFormatter', 'ShowVlanBriefFormatter', 'ShowClockFormatter', 'ShowClockTimezonesFormatter']
